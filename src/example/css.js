869e3f5645da15337279c0b449de827dc6ab82a9
Robert Chang <cht8687@gmail.com>
2015-12-20 15:53:54 +1100

    adjust css of demo

--- src/example/css.js
+++ src/example/css.js
@@ -1,18 +1,21 @@
 const styles = {
   trigger: {
     background: '#E0037E',
-    width: '200px'
+    width: '200px',
+    margin: '0 auto',
+    marginTop: '100px'
   },
 
   hoverComponent: {
-    height: '300px',
+    height: '200px',
     overflowY: 'auto',
     outline: '1px solid blue',
     width: '300px',
     background: '#E8E27E',
-    display: 'none'
+    display: 'none',
+    position: 'absolute',
+    margin: '-20px 0 0 717px'
   }
-  
 };
 
 export default styles;
