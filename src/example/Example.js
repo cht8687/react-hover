58a98603bde9e1b74b5afd6963d1904769e88766
Robert Chang <cht8687@gmail.com>
2017-04-27 23:28:22 +1000

    align file name

--- src/example/Example.js
+++ src/example/Example.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import { render } from 'react-dom'
 import ReactHover from '..'
 import * as styles from './css'
-import HoverComponent  from './hoverComponent';
-import TriggerComponent from './triggerComponent';
+import HoverComponent  from './HoverComponent';
+import TriggerComponent from './TriggerComponent';
 
 const optionsCursorFalse = {
     followCursor: false
