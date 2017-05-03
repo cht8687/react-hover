75c12be51c40ec0d304758cee17d06aca662bc25
Robert Chang <cht8687@gmail.com>
2017-05-03 23:38:10 +1000

    fix test

--- test/index.js
+++ test/index.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import test from 'tape'
 import ReactHover from '../src/ReactHover'
-import TriggerComponent from '../src/lib/TriggerComponent'
-import HoverComponent from '../src/lib/HoverComponent'
-import * as styles from '../src/example/css'
-import * as componentHtml from '../src/example/componentHtml'
+import HoverComponent from '../src/example/HoverComponent'
+import TriggerComponent from '../src/example/TriggerComponent'
 import { shallow, mount } from 'enzyme'
 import sinon from 'sinon'
 
@@ -14,73 +12,14 @@ test('----- React Component Tests: ReactHover -----', t => {
   const optionsCursorFalse = {
    followCursor: false
   }
-  const wrapperShallow = shallow( <ReactHover styles={styles.basic} componentHtml={componentHtml.basicComponentHtml} options={optionsCursorFalse} />)
+  const wrapperShallow = shallow( <ReactHover  options={optionsCursorFalse}>
+              <ReactHover.Trigger>
+                <TriggerComponent />
+              </ReactHover.Trigger>
+              <ReactHover.Hover>
+                <HoverComponent />
+              </ReactHover.Hover> </ReactHover>)
   t.equal(1, wrapperShallow.find('TriggerComponent').length)
   t.equal(1, wrapperShallow.find('HoverComponent').length)
   t.end()
 })
-
-test('----- React Component Tests: TriggerComponent -----', t => {
-  t.plan(2)
-  t.ok(TriggerComponent instanceof Function, 'should be function')
-  const styles = {
-    trigger: {
-      background: '#E0037E',
-      width: '200px',
-      margin: '0 auto'
-    },
-    hoverComponent: {
-      height: '200px',
-      overflowY: 'auto',
-      outline: '1px solid blue',
-      width: '300px',
-      background: '#E8E27E',
-      display: 'none',
-      position: 'absolute',
-      margin: '-20px 0 0 717px'
-    }
-  }
-
-  const componentHtml = {
-    hoverComponent: '<h1> pop up header </h1> <p> pop up content </p>',
-    trigger: 'hover me'
-  }
-
-  function func() {};
-  const wrapper = shallow(<TriggerComponent styles={styles} componentHtml={componentHtml}  setVisibility={func} getCursorPos={func}/>)
-  t.equal(true, wrapper.containsMatchingElement(<div></div>))
-
-  t.end()
-})
-
-test('----- React Component Tests: HoverComponent -----', t => {
-  t.plan(2)
-  t.ok(HoverComponent instanceof Function, 'should be function')
-  const styles = {
-    trigger: {
-      background: '#E0037E',
-      width: '200px',
-      margin: '0 auto'
-    },
-    hoverComponent: {
-      height: '200px',
-      overflowY: 'auto',
-      outline: '1px solid blue',
-      width: '300px',
-      background: '#E8E27E',
-      display: 'none',
-      position: 'absolute',
-      margin: '-20px 0 0 717px'
-    }
-  }
-
-  const componentHtml = {
-    hoverComponent: '<h1> pop up header </h1> <p> pop up content </p>',
-    trigger: 'hover me'
-  }
-
-  const wrapper = shallow(<HoverComponent styles={styles} componentHtml={componentHtml} />)
-  t.equal(true, wrapper.containsMatchingElement(<div></div>))
-  t.end()
-})
-
