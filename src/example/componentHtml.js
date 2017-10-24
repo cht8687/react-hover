e2e0a5e1045df5866334965f00e0efc8b56aff18
cht8687 <cht8687@gmail.com>
2017-10-24 20:45:56 +1100

    (chore)remove unused file

--- src/example/componentHtml.js
+++
@@ -1,9 +0,0 @@
-export const basicComponentHtml = {
-  hoverComponent: '<h1> pop up header </h1> <p> pop up content </p>',
-  trigger: 'hover me'
-}
-
-export const advancedComponentHtml = {
-  hoverComponent: '<h1> Macbook Pro </h1> <img src="./macbook.jpg" style="width: 200px" /><p> It’s the future of the notebook. </p>',
-  trigger: 'macbook'
-}
