132a400a9228a434436d3df07dfc6a8af8553da6
Timothy Lentz <tlentzfilm@gmail.com>
2019-04-19 23:18:33 -0700

    Fixed the blinking issue by adding the setVisability to the hover component.

--- src/lib/Hover.js
+++ src/lib/Hover.js
@@ -5,15 +5,37 @@ export default class Hover extends Component {
   static propTypes = {
     type: PropTypes.string,
     children: PropTypes.object,
-    styles: PropTypes.object
-  }
+    styles: PropTypes.object,
+    setVisibility: PropTypes.func,
+    getCursorPos: PropTypes.func
+  };
 
-  render () {
+  render() {
     const { styles } = this.props.children.props
     return (
-      <div style={styles}>
+      <div
+        onMouseOver={this.onMouseOver.bind(this)}
+        onMouseOut={this.onMouseOut.bind(this)}
+        onMouseMove={this.onMouseMove.bind(this)}
+        style={styles}
+      >
         {this.props.children.props.children}
       </div>
-    )
+    );
+  }
+
+  onMouseOver() {
+    const { setVisibility } = this.props.children.props;
+    setVisibility(true);
+  }
+
+  onMouseOut() {
+    const { setVisibility } = this.props.children.props;
+    setVisibility(false);
+  }
+
+  onMouseMove(e) {
+    const { getCursorPos } = this.props.children.props;
+    getCursorPos(e);
   }
 }
