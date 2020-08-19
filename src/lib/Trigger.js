ef8b1cd700c60165a6df4b86b197debe0206f54a
illiteratewriter <manukoshyabraham@gmail.com>
2020-08-19 11:51:08 +0530

    migrate to hooks
    upgraded react from 16.6.0 to 16.13.1
    migrated class components to functional
    added hooks instead of classes

--- src/lib/Trigger.js
+++ src/lib/Trigger.js
@@ -1,75 +1,45 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import ReactDom from 'react-dom'
 import PropTypes from 'prop-types'
 
-export default class Trigger extends Component {
-  static propTypes = {
-    type: PropTypes.string,
-    children: PropTypes.object,
-    setVisibility: PropTypes.func,
-    getCursorPos: PropTypes.func,
-  }
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      styles: {},
-    }
-  }
-
-  componentDidMount() {
-    // let childStyles = this.refs.triggerContainer.children[0].style
-    let childStyles = window.getComputedStyle(
-      ReactDom.findDOMNode(this.refs.triggerContainer.children[0]),
-    )
-    this.setState({
-      styles: {
-        width: childStyles.getPropertyValue('width'),
-        height: childStyles.getPropertyValue('height'),
-        margin: childStyles.getPropertyValue('margin'),
-      },
-    })
-  }
-
-  render() {
-    const { styles } = this.state
-    return (
-      <div
-        onMouseOver={this.onMouseOver.bind(this)}
-        onMouseOut={this.onMouseOut.bind(this)}
-        onMouseMove={this.onMouseMove.bind(this)}
-        onTouchStart={this.onTouchStart.bind(this)}
-        onTouchEnd={this.onTouchEnd.bind(this)}
-        ref="triggerContainer"
-        style={styles}
-      >
-        {this.props.children.props.children}
-      </div>
-    )
-  }
-
-  onMouseOver() {
-    const { setVisibility } = this.props.children.props
-    setVisibility(true)
-  }
-
-  onMouseOut() {
-    const { setVisibility } = this.props.children.props
-    setVisibility(false)
-  }
-
-  onMouseMove(e) {
-    const { getCursorPos } = this.props.children.props
-    getCursorPos(e)
-  }
-
-  onTouchStart() {
-    const { setVisibility } = this.props.children.props
-    setVisibility(true)
-  }
-
-  onTouchEnd() {
-    const { setVisibility } = this.props.children.props
-    setVisibility(false)
-  }
+const propTypes = {
+  type: PropTypes.string,
+  children: PropTypes.object,
+  setVisibility: PropTypes.func,
+  getCursorPos: PropTypes.func,
 }
+
+function Trigger(props){
+  const [styles, setStyles] = useState({});
+  const { setVisibility, getCursorPos } = props.children.props;
+  const triggerContainerRef = useRef(null)
+
+  useEffect(() => {
+    let childStyles = window.getComputedStyle(
+      ReactDom.findDOMNode(triggerContainerRef.current.children[0]),
+    )
+    setStyles({
+      width: childStyles.getPropertyValue('width'),
+      height: childStyles.getPropertyValue('height'),
+      margin: childStyles.getPropertyValue('margin')
+    })
+  }, [])
+
+  return(
+    <div
+      onMouseOver={() => setVisibility(true)}
+      onMouseOut={() => setVisibility(false)}
+      onMouseMove={(e) => getCursorPos(e)}
+      onTouchStart={() => setVisibility(true)}
+      onTouchEnd={() => setVisibility(false)}
+      ref={triggerContainerRef}
+      style={styles}
+    >
+      {props.children.props.children}
+    </div>
+  )
+}
+
+Trigger.propTypes = propTypes;
+
+export default Trigger;
